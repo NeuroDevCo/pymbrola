5ecca4bb2308bc7441c01e3951d211c9d2b49402
gongcastro <gongarciacastro@gmail.com>
2026-05-18 14:55:51 +0200

    Import functions in init

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,30 @@
 # SPDX-FileCopyrightText: 2024-present gongcastro <gongarciacastro@gmail.com>
 #
 # SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2024-present gongcastro <gongarciacastro@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+from .mbrola import (
+    MBROLA,
+    PlatformException,
+    is_wsl,
+    make_pho,
+    mbrola_cmd,
+    validate_durations,
+    validate_outer_silences,
+    validate_pitch,
+    wsl_available,
+)
+
+__all__ = [
+    "MBROLA",
+    "PlatformException",
+    "is_wsl",
+    "make_pho",
+    "mbrola_cmd",
+    "validate_durations",
+    "validate_outer_silences",
+    "validate_pitch",
+    "wsl_available",
+]
